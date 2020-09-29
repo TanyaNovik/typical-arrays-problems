8b8559e4b67e7e222a599cda4bdc0b997d0f522b
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-09-29 18:05:40 +0300

    init: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if(array === undefined || array.length === 0) {
+        return 0;
+    }
+    let minElement = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minElement) {
+            minElement = array[i];
+        }
+    }
+    return minElement;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if(array === undefined || array.length === 0) {
+        return 0;
+    }
+    let maxElement = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxElement) {
+            maxElement = array[i];
+        }
+    }
+    return maxElement;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if(array === undefined || array.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+       sum += array[i];
+    }
+    let result = sum/array.length;
+    return result;
 }
